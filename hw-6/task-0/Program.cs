1be0fa552b376696bc05b63cf40bfdca68e72ac4
Nikita Goroh <NGoroch2112@gmail.com>
2023-07-11 13:00:56 +0300

    Homework №6 added

---
+++ hw-6/task-0/Program.cs
@@ -0,0 +1,51 @@
+﻿// Задача 1: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+
+int value(string message)
+{
+    Console.WriteLine($"{message} > ");
+    int result = int.Parse(Console.ReadLine());
+    return result;
+}
+int[] PrintArray(int[] array)
+{
+    foreach (int item in array)
+    {
+        System.Console.Write(item + "\t");
+    }
+    System.Console.WriteLine();
+    return array;
+}
+
+
+int[] inputOfNumbers(int len)
+{
+    int[] array = new int[len];
+    for (int i = 0; i < array.Length; i++)
+    {
+        int input = value("введите число -> ");
+        array[i] = input;
+    }
+    return array;
+}
+
+int sumOfPositive(int[] array)
+{
+    int count = 0;
+    foreach (int item in array)
+    {
+        if (item > 0)
+        {
+            count += item;
+        }
+    }
+    return count;
+}
+
+int countNumbers = value("сколько чисел вы хотите ввести");
+int[] num = inputOfNumbers(countNumbers);
+PrintArray(num);
+int sum = sumOfPositive(num);
+System.Console.WriteLine($"Сумма положительных равна {sum}");
---
+++ hw-6/task-1/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 2: Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9-> (-0, 5; -0,5)
+
+
+int value(string message)
+{
+    Console.WriteLine($"{message} > ");
+    int result = int.Parse(Console.ReadLine());
+    return result;
+}
+
+int b1 = value("введите значение b1");
+int k1 = value("введите значение k1");
+int b2 = value("введите значение b2");
+int k2 = value("введите значение k2");
+
